a9356ee4d2bf88123ffba1d74aa1515d8f0cbee0
rhyperio <rhyperio.yt@gmail.com>
2022-11-24 17:03:31 +0100

    Add first test for correct top down parsing solution

---
+++ src/test/java/de/dhbw/karlsruhe/top/down/parsing/TopDownAcceptorValidationTest.java
@@ -0,0 +1,33 @@
+package de.dhbw.karlsruhe.top.down.parsing;
+
+import com.google.gson.Gson;
+import de.dhbw.karlsruhe.top.down.parsing.models.TopDownAcceptor;
+import de.dhbw.karlsruhe.top.down.parsing.validation.TopDownAcceptorValidation;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TopDownAcceptorValidationTest {
+
+    @Test
+    public void correctTopDownParsingTest() throws FileNotFoundException {
+        String grammarAsJson = getGrammarAsJson("src/test/resources/top_down_parsing/TopDownCorrect.json");
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("src/test/resources/bottom_up/bUAcceptorCorrect1.json")) {
+            TopDownAcceptor tDAcceptor = gson.fromJson(reader, TopDownAcceptor.class);
+            TopDownAcceptorValidation tDAcceptorValidation = new TopDownAcceptorValidation(grammarAsJson);
+            String word = "()()";
+            assertTrue(tDAcceptorValidation.validateTopDownAcceptor(tDAcceptor, word));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getGrammarAsJson(String path) throws FileNotFoundException {
+        return new Scanner(new File(path)).useDelimiter("\\Z").next();
+    }
+}
