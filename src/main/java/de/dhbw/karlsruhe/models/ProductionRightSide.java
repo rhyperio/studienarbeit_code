3c702f9a152e03c55c6dad4b642c2af176928771
Alisa Fien <alisa1fien@gmail.com>
2023-02-21 09:59:48 +0100

    Removed unused parameter of ProductionRightSide

--- src/main/java/de/dhbw/karlsruhe/models/ProductionRightSide.java
+++ src/main/java/de/dhbw/karlsruhe/models/ProductionRightSide.java
@@ -4,23 +4,21 @@ import java.util.List;
 import java.util.Random;
 
 public enum ProductionRightSide {
-    p1("t", true),
-    p2("N", false),
-    p3("t N", false),
-    p4("t t N", false),
-    p5("epsilon", true);
+    p1("t"),
+    p2("N"),
+    p3("t N"),
+    p4("t t N"),
+    p5("epsilon");
 
     private static final List<ProductionRightSide> VALUES =
             List.of(values());
     private static final int SIZE = VALUES.size();
     private static final Random RANDOM = new Random();
     public final String rightSide;
-    public final boolean isEndProduction;
 
 
-    ProductionRightSide(String rightSide, boolean isEndProduction) {
+    ProductionRightSide(String rightSide) {
         this.rightSide = rightSide;
-        this.isEndProduction = isEndProduction;
     }
     public static ProductionRightSide randomProduction(){
         return VALUES.get(RANDOM.nextInt(SIZE));
