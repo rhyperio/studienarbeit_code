7ae4c5cd5ff8eae83a281bc7510624c6c916d4d7
mohjohfox <marcofuchs@mohjohfox.de>
2023-03-09 18:50:57 +0100

    do not simplify grammar rules with or notation

--- src/main/java/de/dhbw/karlsruhe/grammar/generation/GrammarGeneration.java
+++ src/main/java/de/dhbw/karlsruhe/grammar/generation/GrammarGeneration.java
@@ -1,119 +1,122 @@
 package de.dhbw.karlsruhe.grammar.generation;
 
-import de.dhbw.karlsruhe.models.Grammar;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.RandomStringUtils;
+
+import de.dhbw.karlsruhe.models.Grammar;
 
 public abstract class GrammarGeneration {
 
-  List<String> terminals = new ArrayList<>();
-  List<String> nonTerminals = new ArrayList<>();
-  List<String> usableTerminals = new ArrayList<>();
-  List<String> usableNonTerminals = new ArrayList<>();
-  String startSymbol;
+	List<String> terminals = new ArrayList<>();
+	List<String> nonTerminals = new ArrayList<>();
+	List<String> usableTerminals = new ArrayList<>();
+	List<String> usableNonTerminals = new ArrayList<>();
+	String startSymbol;
 
-  public abstract Grammar generateGrammar();
-  protected abstract List<String>  generateProductions();
+	public abstract Grammar generateGrammar();
 
-  List<String> cleanProductions(List<String> generatedProductions) {
-    return formatProductions(generatedProductions.stream().distinct().collect(Collectors.toList()));
-  }
+	protected abstract List<String> generateProductions();
 
-  boolean isLoop(String startSymbol, List<String> productions, String leftSide, char rightSide) {
-    List<String> productionsWithRightSideOnLeft = new ArrayList<>();
-    productions.forEach(production -> {
-      if (production.substring(production.indexOf('>') + 1 ).contains(leftSide)) {
-        productionsWithRightSideOnLeft.add(production);
-      }
-    });
+	List<String> cleanProductions(List<String> generatedProductions) {
+		return generatedProductions.stream().distinct().collect(Collectors.toList());
+	}
 
-    for (String production : productionsWithRightSideOnLeft) {
-      if (production.charAt(0) == rightSide) {
-        return true;
-      } else if (production.charAt(0) == startSymbol.charAt(0) || production.charAt(0) == leftSide.charAt(0)) {
-        return true;
-      } else {
-        return isLoop(startSymbol, productions, String.valueOf(production.charAt(0)), rightSide);
-      }
-    }
-    return false;
-  }
+	boolean isLoop(String startSymbol, List<String> productions, String leftSide, char rightSide) {
+		List<String> productionsWithRightSideOnLeft = new ArrayList<>();
+		productions.forEach(production -> {
+			if (production.substring(production.indexOf('>') + 1).contains(leftSide)) {
+				productionsWithRightSideOnLeft.add(production);
+			}
+		});
 
-  String buildProduction(String leftSide, String rightSide) {
-    return leftSide + " -> " + rightSide;
-  }
+		for (String production : productionsWithRightSideOnLeft) {
+			if (production.charAt(0) == rightSide) {
+				return true;
+			} else if (production.charAt(0) == startSymbol.charAt(0) || production.charAt(0) == leftSide.charAt(0)) {
+				return true;
+			} else {
+				return isLoop(startSymbol, productions, String.valueOf(production.charAt(0)), rightSide);
+			}
+		}
+		return false;
+	}
 
-  List<String> generateTerminals() {
-    List<String> terminals = new ArrayList<>();
-    Random rand = new Random();
-    while (terminals.size() < 5) {
-      if (rand.nextFloat() <= 0.1 || terminals.contains("ε")){
-        String terminal = RandomStringUtils.randomAlphabetic(1).toLowerCase();
-        if (!terminals.contains(terminal)) {
-          terminals.add(terminal);
-        }
-      } else {
-        terminals.add("ε");
-      }
-    }
-    return terminals;
-  }
+	String buildProduction(String leftSide, String rightSide) {
+		return leftSide + " -> " + rightSide;
+	}
 
-  List<String> generateNonTerminals() {
-    List<String> generatedNonTerminals = new ArrayList<>();
-    while (generatedNonTerminals.size() < 5) {
-      String nonTerminal = RandomStringUtils.randomAlphabetic(1).toUpperCase();
-      if (!generatedNonTerminals.contains(nonTerminal)) {
-        generatedNonTerminals.add(nonTerminal);
-      }
-    }
-    return generatedNonTerminals;
-  }
+	List<String> generateTerminals() {
+		List<String> terminals = new ArrayList<>();
+		Random rand = new Random();
+		while (terminals.size() < 5) {
+			if (rand.nextFloat() <= 0.1 || terminals.contains("ε")) {
+				String terminal = RandomStringUtils.randomAlphabetic(1).toLowerCase();
+				if (!terminals.contains(terminal)) {
+					terminals.add(terminal);
+				}
+			} else {
+				terminals.add("ε");
+			}
+		}
+		return terminals;
+	}
 
-  void addToTerminals(String terminal) {
-    if (!terminals.contains(terminal)) {
-      terminals.add(terminal);
-    }
-  }
+	List<String> generateNonTerminals() {
+		List<String> generatedNonTerminals = new ArrayList<>();
+		while (generatedNonTerminals.size() < 5) {
+			String nonTerminal = RandomStringUtils.randomAlphabetic(1).toUpperCase();
+			if (!generatedNonTerminals.contains(nonTerminal)) {
+				generatedNonTerminals.add(nonTerminal);
+			}
+		}
+		return generatedNonTerminals;
+	}
 
-  void addNonTerminal(String nonTerminal) {
-    if (!nonTerminals.contains(nonTerminal)) {
-      nonTerminals.add(nonTerminal);
-    }
-  }
+	void addToTerminals(String terminal) {
+		if (!terminals.contains(terminal)) {
+			terminals.add(terminal);
+		}
+	}
 
-  private List<String> formatProductions(List<String> generatedProductions) {
-    List<String> cleanedProductions = new ArrayList<>();
+	void addNonTerminal(String nonTerminal) {
+		if (!nonTerminals.contains(nonTerminal)) {
+			nonTerminals.add(nonTerminal);
+		}
+	}
 
-    for (String currProduction : generatedProductions) {
-      char first = currProduction.charAt(0);
-      if (isNotAlreadyInList(cleanedProductions, first)) {
-        StringBuilder currProductionBuilder = new StringBuilder(currProduction);
-        for (String production: generatedProductions) {
-          if (currProduction.equals(production)) {
-            continue;
-          }
-          char second = production.charAt(0);
-          if (first == second) {
-            currProductionBuilder.append(" | ").append(production.substring(production.indexOf('>') + 1));
-          }
-        }
-        cleanedProductions.add(currProductionBuilder.toString());
-      }
-    }
-    return cleanedProductions;
-  }
+	private List<String> formatProductions(List<String> generatedProductions) {
+		List<String> cleanedProductions = new ArrayList<>();
 
-  private boolean isNotAlreadyInList(List<String> productions, char leftSide) {
-    for (String currProd: productions) {
-      if (currProd.charAt(0) == leftSide) {
-        return false;
-      }
-    }
-    return true;
-  }
+		for (String currProduction : generatedProductions) {
+			char first = currProduction.charAt(0);
+			if (isNotAlreadyInList(cleanedProductions, first)) {
+				StringBuilder currProductionBuilder = new StringBuilder(currProduction);
+				for (String production : generatedProductions) {
+					if (currProduction.equals(production)) {
+						continue;
+					}
+					char second = production.charAt(0);
+					if (first == second) {
+						currProductionBuilder.append(" | ").append(production.substring(production.indexOf('>') + 1));
+					}
+				}
+				cleanedProductions.add(currProductionBuilder.toString());
+			}
+		}
+		return cleanedProductions;
+	}
+
+	private boolean isNotAlreadyInList(List<String> productions, char leftSide) {
+		for (String currProd : productions) {
+			if (currProd.charAt(0) == leftSide) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
