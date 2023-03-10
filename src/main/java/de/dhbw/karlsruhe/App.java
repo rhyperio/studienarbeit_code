7133967ccbde58443f35c877da663f1162c8ddd3
mohjohfox <marcofuchs@mohjohfox.de>
2023-03-10 22:14:40 +0100

    Added needed dependencies and classes

--- src/main/java/de/dhbw/karlsruhe/App.java
+++ src/main/java/de/dhbw/karlsruhe/App.java
@@ -10,10 +10,14 @@ import de.dhbw.karlsruhe.util.Resource;
 
 
 import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
 
     Resource resource = new Resource();
     String grammarAsJson = "";
