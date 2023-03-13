0ef00d1e616fa4a42bbc04a07ac6b2a4899f6936
mohjohfox <marcorobinf@gmail.com>
2023-03-13 15:07:56 +0100

    added endpoint for grammar pattern generation

--- src/main/java/de/dhbw/karlsruhe/controller/GrammarGenerationController.java
+++ src/main/java/de/dhbw/karlsruhe/controller/GrammarGenerationController.java
@@ -1,13 +1,13 @@
 package de.dhbw.karlsruhe.controller;
 
 import de.dhbw.karlsruhe.grammar.generation.GrammarGeneration;
+import de.dhbw.karlsruhe.grammar.generation.GrammarPatternProductionsGeneration;
 import de.dhbw.karlsruhe.grammar.generation.GrammarProbabilityGeneration;
 import de.dhbw.karlsruhe.models.Grammar;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @CrossOrigin
 @org.springframework.web.bind.annotation.RestController
@@ -16,6 +16,14 @@ public class GrammarGenerationController {
     @GetMapping("/api/get/grammar/probability")
     ResponseEntity<Grammar> getProbabilityGeneratedGrammar() {
         GrammarGeneration grammarGeneration = new GrammarProbabilityGeneration();
+        Grammar generatedGrammar = grammarGeneration.generateGrammar();
+        generatedGrammar.mergeOrGrammarsIntoSingleRules();
+        return new ResponseEntity<>(generatedGrammar, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/get/grammar/pattern")
+    ResponseEntity<Grammar> getPatternGeneratedGrammar() {
+        GrammarGeneration grammarGeneration = new GrammarPatternProductionsGeneration();
         Grammar generatedGrammar = grammarGeneration.generateGrammar();
         generatedGrammar.mergeOrGrammarsIntoSingleRules();
         return new ResponseEntity<>(generatedGrammar, HttpStatus.OK);
