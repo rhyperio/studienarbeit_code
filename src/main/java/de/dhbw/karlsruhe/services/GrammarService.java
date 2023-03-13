b0143ee817bb03dc66eded0e8d3026612751045f
mohjohfox <marcorobinf@gmail.com>
2023-03-13 15:04:35 +0100

    fromat grammar in test cases

--- src/main/java/de/dhbw/karlsruhe/services/GrammarService.java
+++ src/main/java/de/dhbw/karlsruhe/services/GrammarService.java
@@ -14,6 +14,7 @@ public class GrammarService {
 
   public GrammarService(String json) {
     grammar = formatGrammar(json);
+    grammar.splitOrGrammarsIntoSingleRules();
   }
 
   public GrammarService(Grammar grammar) {
