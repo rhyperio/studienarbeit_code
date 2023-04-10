89bda03fff231c7efde9da6b4cc8619503183b53
mohjohfox <marcorobinf@gmail.com>
2023-04-10 10:50:16 +0200

    added first test data and correct reading input from test data

--- src/test/java/de/dhbw/karlsruhe/derivation/leftRight/SpecificDerivationValidationTest.java
+++ src/test/java/de/dhbw/karlsruhe/derivation/leftRight/SpecificDerivationValidationTest.java
@@ -1,11 +1,13 @@
 package de.dhbw.karlsruhe.derivation.leftRight;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import de.dhbw.karlsruhe.models.Grammar;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,7 +17,7 @@ public class SpecificDerivationValidationTest {
     @Test
     public void correctLeftDerivation() throws FileNotFoundException {
         Grammar grammar = getGrammar(
-                "src/test/resources/derivation_tree/grammarCorrect1.json");
+                "src/test/resources/derivation/grammarCorrect1.json");
         List<String> derivationList = getDerivationList(
                 "src/test/resources/derivation/derivation1.json");
 
@@ -26,7 +28,7 @@ public class SpecificDerivationValidationTest {
     @Test
     public void correctRightDerivation() throws FileNotFoundException {
         Grammar grammar = getGrammar(
-                "src/test/resources/derivation_tree/grammarCorrect1.json");
+                "src/test/resources/derivation/grammarCorrect1.json");
         List<String> derivationList = getDerivationList(
                 "src/test/resources/derivation/derivation1.json");
 
@@ -37,7 +39,7 @@ public class SpecificDerivationValidationTest {
     @Test
     public void wrongLeftDerivation() throws FileNotFoundException {
         Grammar grammar = getGrammar(
-                "src/test/resources/derivation_tree/grammarCorrect1.json");
+                "src/test/resources/derivation/grammarCorrect1.json");
         List<String> derivationList = getDerivationList(
                 "src/test/resources/derivation/derivation1.json");
 
@@ -48,7 +50,7 @@ public class SpecificDerivationValidationTest {
     @Test
     public void wrongRightDerivation() throws FileNotFoundException {
         Grammar grammar = getGrammar(
-                "src/test/resources/derivation_tree/grammarCorrect1.json");
+                "src/test/resources/derivation/grammarCorrect1.json");
         List<String> derivationList = getDerivationList(
                 "src/test/resources/derivation/derivation1.json");
 
@@ -61,6 +63,8 @@ public class SpecificDerivationValidationTest {
     }
 
     private List<String> getDerivationList(String path) throws FileNotFoundException {
-        return new Scanner(new File(path)).useDelimiter("\\Z").next();
+        String input = new Scanner(new File(path)).useDelimiter("\\Z").next();
+        String derivations = new Gson().fromJson(input, JsonObject.class).get("derivation").getAsString();
+        return Arrays.stream(derivations.split("->")).map(String::trim).toList();
     }
 }
