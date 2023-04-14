fb506d3b8141579170a5b7b2bb4e9fcf1eef6003
Marcel <rhyperio.yt@gmail.com>
2023-04-14 09:00:38 +0200

    Add algorithm to check if any grammar is in cnf

---
+++ src/main/java/de/dhbw/karlsruhe/grammar/validation/ValidateGrammarCNF.java
@@ -0,0 +1,83 @@
+package de.dhbw.karlsruhe.grammar.validation;
+
+import de.dhbw.karlsruhe.models.Grammar;
+import de.dhbw.karlsruhe.models.GrammarProduction;
+import de.dhbw.karlsruhe.services.GrammarService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ValidateGrammarCNF {
+
+    public boolean validateGrammarIsInCNF(Grammar grammarToCheck) {
+        List<GrammarProduction> grammarProductions = Arrays.asList(grammarToCheck.getProductions());
+        String[] nonTerminals = grammarToCheck.getNonTerminals();
+        return this.rightSideExactlyTwoNonTerminals(grammarProductions, nonTerminals) && this.rightSideExactlyOneTerminal(grammarProductions) && this.maxOneEpsilonProductionNoRightSide(grammarProductions);
+    }
+
+    private boolean rightSideExactlyTwoNonTerminals(List<GrammarProduction> grammarProductions, String[] nonTerminals) {
+        boolean valid = false;
+
+        for (GrammarProduction gp : grammarProductions) {
+            for (int i = 0; i < gp.rightSide().length(); i++) {
+                StringBuilder nonTerminalToCheck = new StringBuilder(String.valueOf(gp.rightSide().charAt(i)));
+
+                if (nonTerminalToCheck.toString().equals("Z") || nonTerminalToCheck.toString().equals("V")) {
+                    do {
+                        nonTerminalToCheck.append(String.valueOf(gp.rightSide().charAt(i + 1)));
+                        i += 1;
+                    } while (Character.isDigit(gp.rightSide().charAt(i+1)));
+                }
+
+                valid = Arrays.stream(nonTerminals).anyMatch(nonTerminalToCheck.toString()::contains);
+
+                if (!valid) {
+                    return false;
+                }
+            }
+
+        }
+
+        return valid;
+    }
+
+    private boolean rightSideExactlyOneTerminal(List<GrammarProduction> grammarProductions) {
+        for (GrammarProduction gp : grammarProductions) {
+            if (gp.rightSide().length() == 1) {
+                if (!Character.isLowerCase(gp.rightSide().charAt(0))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean maxOneEpsilonProductionNoRightSide(List<GrammarProduction> grammarProductions) {
+        int anzEpsilonProductions = 0;
+        String leftSideOfEpsilonProduction = "";
+
+        for (GrammarProduction gp : grammarProductions) {
+            if (gp.rightSide().equals("ε")) {
+                anzEpsilonProductions ++;
+                leftSideOfEpsilonProduction = gp.leftSide();
+            }
+        }
+
+        if (anzEpsilonProductions > 1) {
+            return false;
+        }
+
+        return this.checkEpsilonProductionIsOnNoRightSide(grammarProductions, leftSideOfEpsilonProduction);
+    }
+
+    private boolean checkEpsilonProductionIsOnNoRightSide(List<GrammarProduction> grammarProductions, String leftSideOfEpsilonProduction) {
+        for (GrammarProduction gp : grammarProductions) {
+            if (gp.rightSide().contains(leftSideOfEpsilonProduction)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
