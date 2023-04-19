6ffc841b3dedbd58301ae58ff0fc131b63c12688
Marcel <rhyperio.yt@gmail.com>
2023-04-19 08:13:21 +0200

    Clean up

--- src/main/java/de/dhbw/karlsruhe/chomsky/validation/ValidateGrammarCNF.java
+++ src/main/java/de/dhbw/karlsruhe/chomsky/validation/ValidateGrammarCNF.java
@@ -2,7 +2,6 @@ package de.dhbw.karlsruhe.chomsky.validation;
 
 import de.dhbw.karlsruhe.models.Grammar;
 import de.dhbw.karlsruhe.models.GrammarProduction;
-import de.dhbw.karlsruhe.services.GrammarService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,48 +12,9 @@ public class ValidateGrammarCNF {
 
     public boolean validateGrammarIsInCNF(Grammar grammarToCheck) {
         List<GrammarProduction> grammarProductions = Arrays.asList(grammarToCheck.getProductions());
-        String[] nonTerminals = grammarToCheck.getNonTerminals();
         String[] terminals = grammarToCheck.getTerminals();
         return this.rightSideExactlyTwoNonTerminals(grammarProductions, terminals) && this.rightSideExactlyOneTerminal(grammarProductions, terminals) && this.maxOneEpsilonProductionNoRightSide(grammarProductions);
     }
-
-    /**
-    private boolean rightSideExactlyTwoNonTerminals(List<GrammarProduction> grammarProductions, String[] nonTerminals) {
-        boolean valid = false;
-
-        for (GrammarProduction gp : grammarProductions) {
-            if (gp.rightSide().length() == 1) {
-                valid = true;
-            } else {
-                int loops = 0;
-                for (int i = 0; i < gp.rightSide().length(); i++) {
-                    StringBuilder nonTerminalToCheck = new StringBuilder(String.valueOf(gp.rightSide().charAt(i)));
-
-                    if (nonTerminalToCheck.toString().equals("Z") || nonTerminalToCheck.toString().equals("V")) {
-                        do {
-                            nonTerminalToCheck.append(String.valueOf(gp.rightSide().charAt(i + 1)));
-                            i += 1;
-                        } while (Character.isDigit(gp.rightSide().charAt(i+1)));
-                    }
-
-                    valid = Arrays.stream(nonTerminals).anyMatch(nonTerminalToCheck.toString()::contains);
-
-                    if (!valid) {
-                        return false;
-                    }
-
-                    loops++;
-
-                    if (loops > 2) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return valid;
-    }
-     */
 
     private boolean rightSideExactlyTwoNonTerminals(List<GrammarProduction> grammarProductions, String[] terminals) {
         boolean valid = false;
--- src/test/java/de/dhbw/karlsruhe/chomsky/validation/ValidateGrammarCNFTest.java
+++ src/test/java/de/dhbw/karlsruhe/chomsky/validation/ValidateGrammarCNFTest.java
@@ -1,6 +1,5 @@
 package de.dhbw.karlsruhe.chomsky.validation;
 
-import com.google.gson.Gson;
 import de.dhbw.karlsruhe.models.Grammar;
 import de.dhbw.karlsruhe.services.GrammarService;
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,6 @@ public class ValidateGrammarCNFTest {
     public void grammarInCNF() throws FileNotFoundException {
         ValidateGrammarCNF validateGrammarCNF = new ValidateGrammarCNF();
         String grammarAsJson = getGrammarAsJson("src/test/resources/chomsky/grammarChomskyCorrect.json");
-        Gson gson = new Gson();
 
         GrammarService grammarService = new GrammarService(grammarAsJson);
         Grammar grammarToCheck = grammarService.getParsedGrammar();
@@ -28,7 +26,6 @@ public class ValidateGrammarCNFTest {
     public void grammarNotInCNF() throws FileNotFoundException {
         ValidateGrammarCNF validateGrammarCNF = new ValidateGrammarCNF();
         String grammarAsJson = getGrammarAsJson("src/test/resources/chomsky/grammarChomskyFalse.json");
-        Gson gson = new Gson();
 
         GrammarService grammarService = new GrammarService(grammarAsJson);
         Grammar grammarToCheck = grammarService.getParsedGrammar();
