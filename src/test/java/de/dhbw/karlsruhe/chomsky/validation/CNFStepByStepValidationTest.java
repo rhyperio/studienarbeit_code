0c8ecdb597cda788151cb3575cd701629870e934
Marcel <rhyperio.yt@gmail.com>
2023-04-24 23:32:36 +0200

    Add test class for step by step validation

---
+++ src/test/java/de/dhbw/karlsruhe/chomsky/validation/CNFStepByStepValidationTest.java
@@ -0,0 +1,14 @@
+package de.dhbw.karlsruhe.chomsky.validation;
+
+import de.dhbw.karlsruhe.chomsky.generation.ChomskyTransformationGeneration;
+import de.dhbw.karlsruhe.models.Grammar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CNFStepByStepValidationTest {
+    @Test
+    public void checkStepByStepValidation() {
+        
+    }
+}
